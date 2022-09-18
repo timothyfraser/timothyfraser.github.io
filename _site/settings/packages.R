5279319ce344e7d9216c6420a45d102a78462652
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2022-09-18 03:03:59 +0000

    nope, new try

--- _site/settings/packages.R
+++
@@ -1,9 +0,0 @@
-# packages.R
-
-# Install these packages to run related files in this repository
-install.packages(
-  c("tidyverse",  "sf",
-    "fontawesome", "magick", "viridis", "ggtext", "ggpubr", 
-    "flexdashboard", "rmdformats", "blogdown",
-    "credentials", "gert")
-)
